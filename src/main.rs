f94318074e8cec90f1541f0f4c5608d2da6df46a
Per Næss <perness3009@gmail.com>
2023-08-01 19:03:42 +0200

    feat: put logic into function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 fn main() {
+    let fizzbuzz = fizz_buzz(100);
+    for val in fizzbuzz {
+        print!("{}", val);
+    }
+}
+
+fn fizz_buzz(i: u32) -> Vec<String> {
+    let mut fizzbuzz = Vec::new();
+
     let fizz = "Fizz";
     let buzz = "Buzz";
 
-    for i in 1..100 {
+    for i in 1..i {
         if i % 3 == 0 && i % 5 == 0 {
-            print!("{}{} ", fizz, buzz)
+            fizzbuzz.push(format!("{}{} ", fizz, buzz));
         } else if i % 3 == 0 {
-            print!("{} ", fizz);
+            fizzbuzz.push(format!("{} ", fizz));
         } else if i % 5 == 0 {
-            print!("{} ", buzz);
+            fizzbuzz.push(format!("{} ", buzz));
         } else {
-            print!("{} ", i);
+            fizzbuzz.push(format!("{} ", i));
         }
     }
+
+    fizzbuzz
 }
