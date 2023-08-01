50040ac1f3e2296530287a3b2d8a0d5c5ab65be3
Per Næss <perness3009@gmail.com>
2023-08-01 18:36:33 +0200

    feat: add small logic for fizz buzz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 0..100 {
+        if i % 2 == 0 {
+            println!("{} Fizz", i);
+        } else {
+            println!("{} Buzz", i);
+        }
+    }
 }
