ac2e46065b8c2572e5505ea8ba6f4380c073bd34
Per Næss <perness3009@gmail.com>
2023-08-01 18:47:42 +0200

    feat: add more logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    for i in 0..100 {
-        if i % 2 == 0 {
-            println!("{} Fizz", i);
+    let fizz = "Fizz";
+    let buzz = "Buzz";
+
+    for i in 1..100 {
+        if i % 3 == 0 && i % 5 == 0 {
+            print!("{}{} ", fizz, buzz)
+        } else if i % 3 == 0 {
+            print!("{} ", fizz);
+        } else if i % 5 == 0 {
+            print!("{} ", buzz);
         } else {
-            println!("{} Buzz", i);
+            print!("{} ", i);
         }
     }
 }
